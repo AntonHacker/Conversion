42d5cfde981f09a60995a46a83a4ad5befafb916
Unknown <9771394@gmail.com>
2017-02-09 21:03:55 +0300

    Initial commit

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string Google;
+                int A;
+                Console.WriteLine("Введите любое целое число");
+                Google = Console.ReadLine();
+                Int32.TryParse(Google, out A);
+                A = A * A;
+                Console.WriteLine("Sqare is" + A);
+
+            }
+            
+        } 
+    }
+}
